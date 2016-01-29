072b4f23d46a3cb7f7cab790a5598e7bdb3d3bc9
Sean Ho <seanho@Seans-MacBook-Pro.local>
2016-01-29 13:49:19 -0800

    removed mongodb and flattened client directory

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 5000;
 var bodyParser   = require('body-parser');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 
 //config
 //var configDB = require('../config/database.js');
@@ -11,8 +11,8 @@ var mongoose = require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})) // get information from html forms
-app.use(express.static(__dirname + '/../client/public'));
-app.set('views', __dirname + '/../client/views');
+app.use(express.static(__dirname + '/../client'));
+app.set('views', __dirname + '/../client');
 app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'ejs');
 

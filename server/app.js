4e3150a1d07a689a0af1466605512f86b52a1038
Sean Ho <seanthaoho@gmail.com>
2020-05-20 20:30:28 -0700

    remove webpack and update client endpoints

--- server/app.js
+++ server/app.js
@@ -11,8 +11,8 @@ var bodyParser   = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})) // get information from html forms
-app.use(express.static(__dirname + '/../client/lib'));
-app.set('views', __dirname + '/../client/lib');
+app.use(express.static(__dirname + '/../client/src'));
+app.set('views', __dirname + '/../client/src');
 app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'ejs');
 
